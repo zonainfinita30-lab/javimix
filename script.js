c01e6168d08214c90a2b503d8eec283cae6268f3
zonainfinita30-lab <zonainfinita3.0@gmail.com>
2026-03-08 17:09:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleServices(){
+
+const services = document.getElementById("services");
+
+if(services.style.display === "block"){
+services.style.display = "none";
+}else{
+services.style.display = "block";
+}
+
+}
